9edf8b06ab569e461d8d6e518cca8d63920e628a
HadyKh <36478378+HadyKh@users.noreply.github.com>
2021-02-08 16:38:10 +0200

    Create MigratoryBirds.cs

---
+++ Migratory birds/MigratoryBirds.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<int> arr) {
+        int[] a = new int[6] { 0,0,0,0,0,0};
+        for (int i = 0; i < arr.Count; i++)
+        {
+            a[arr[i]]++;
+        }
+        
+        int biggest=0;
+        int max = a[1];
+        for (int i = 0; i < 6; i++)
+        {
+            if (a[i] > max)
+            {
+                max = a[i];
+                biggest = i;
+            }
+        }
+        
+        return biggest;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
